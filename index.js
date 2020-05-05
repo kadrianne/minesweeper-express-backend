ffdc173f7a4c34fb1e96abf710ffcafdf28348f1
kadrianne <kristine.a.du@gmail.com>
2020-05-05 11:34:07 -0600

    Enable cors.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const scoresRoutes = require('./routes/scores')
 
 const port = process.env.PORT || 4000
 
+app.use(cors())
 app.use('/scores', scoresRoutes)
 
 app.listen(port)
