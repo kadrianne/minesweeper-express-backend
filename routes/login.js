4f51db3f861cea36384bb6b24f541f3208687afb
kadrianne <kristine.a.du@gmail.com>
2020-05-06 17:52:47 -0600

    Assign token on login.

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,13 @@ const app = express()
 const database = require('../models/database-config')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const secretKey = require('crypto').randomBytes(64).toString('hex')
 
 app.use(express.json())
+
+function generateToken(payload){
+    return jwt.sign(payload,secretKey)
+}
 
 app.post('/', (request,response) => {
     const username = request.body.username
@@ -14,19 +19,21 @@ app.post('/', (request,response) => {
     database('users').select('*').where('username',username)
         .then(foundUser => {
             if (foundUser[0]) {
-                checkPassword(foundUser[0].password)
+                checkPassword(foundUser[0],foundUser[0].password)
             } else {
-                response.status(401).json({status: 401, message: 'Username not found.'})
+                response.status(401).json({status: '401', message: 'Username not found.'})
             }
         })
 
-    async function checkPassword(hash){
+    async function checkPassword(user,hash){
         const match = await bcrypt.compare(password, hash)
-
+        const payload = {id: user.id, display_name: user.display_name}
+        
         if (match) {
-            response.status(202).json({status: 202, message: 'User logged in.'})
+            const token = generateToken(payload)
+            response.status(202).json({status: '202', message: 'User logged in.', token: token})
         } else {
-            response.status(401).json({status: 401, message: 'Password incorrect.'})
+            response.status(401).json({status: '401', message: 'Password incorrect.'})
         }
     }
 })
