26647c0ad919ea2b523d0704b7e814d037aa0bfc
kadrianne <kristine.a.du@gmail.com>
2020-05-06 15:13:45 -0600

    Add validation on user creation.

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const app = express()
 const database = require('../models/database-config')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+// const { check, validationResult } = require('express-validator');
 
 app.use(express.json())
 
@@ -12,14 +13,24 @@ app.get('/', (request,response) => {
 })
 
 app.post('/', (request,response) => {
-    hashedPassword = bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(request.body.password, salt, function(err, hash) {
-            let userData = {...request.body, password: hash}
+    database('users').select('username').where('username',request.body.username)
+        .then(foundUsers => {
+            if (foundUsers.length === 0){
+                hashedPassword = bcrypt.genSalt(saltRounds, function(err, salt) {
+                    bcrypt.hash(request.body.password, salt, function(err, hash) {
+                        let userData = {...request.body, password: hash}
+            
+                        database('users').insert(userData).returning('*')
+                            .then(user => response.json({user: user, status: 201}))
 
-            database.insert(userData).returning('*').into('users')
-                .then(user => response.json({user}))
+                        response.status(201)
+                    })
+                })
+            } else {
+                response.status(409)
+                response.json({status: 409, message: 'Username already exists.'})
+            }
         })
-    })
 })
 
 module.exports = app
