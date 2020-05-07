2c28669ea0a69f19b0ab370d514f470828ce7fae
kadrianne <kristine.a.du@gmail.com>
2020-05-07 10:01:33 -0600

    Create models and relationships with Objection.

--- routes/users.js
+++ routes/users.js
@@ -5,22 +5,29 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 // const { check, validationResult } = require('express-validator');
 
+const User = require('../models/User')
+
 app.use(express.json())
 
 app.get('/', (request,response) => {
-    database('users').select()
+    User.query().withGraphFetched('scores').select()
+        .then(scores => response.json(scores))
+})
+
+app.get(`/:id`, (request,response) => {
+    User.query().where('id',request.params.id).withGraphFetched('scores').select()
         .then(scores => response.json(scores))
 })
 
 app.post('/', (request,response) => {
-    database('users').select('username').where('username',request.body.username)
+    User.query().select('username').where('username',request.body.username)
         .then(foundUsers => {
             if (foundUsers.length === 0){
                 hashedPassword = bcrypt.genSalt(saltRounds, function(err, salt) {
                     bcrypt.hash(request.body.password, salt, function(err, hash) {
                         let userData = {...request.body, password: hash}
             
-                        database('users').insert(userData).returning('*')
+                        User.query().insert(userData).returning('*')
                             .then(user => response.status(201).json({user: user, status: '201', message: 'User successfully created.'}))
                     })
                 })
