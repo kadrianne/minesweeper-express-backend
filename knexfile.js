5a89b755bbc749eda5913da5bd335779f81ae6ab
kadrianne <kristine.a.du@gmail.com>
2020-06-10 15:43:01 -0600

    Update production configs.

--- knexfile.js
+++ knexfile.js
@@ -9,18 +9,10 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL,
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
   }
 
 };
