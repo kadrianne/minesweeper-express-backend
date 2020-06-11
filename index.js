2778cff467c838072f412c8788545cd88a2f452b
kadrianne <kristine.a.du@gmail.com>
2020-06-11 11:41:12 -0600

    Update cors options.

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const loginRoutes = require('./routes/login')
 
 const port = process.env.PORT || 4000
 
-app.use(cors())
-app.options('*', cors())
+app.use(cors({credentials: true, origin: true}))
 app.use(express.json())
 app.use('/scores', scoresRoutes)
 app.use('/highscores', highScoresRoutes)
