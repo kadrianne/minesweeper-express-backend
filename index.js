da9c413aaf1bff133c55a99fed15a2b30939245e
kadrianne <kristine.a.du@gmail.com>
2020-06-11 11:36:17 -0600

    Update cors options.

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ const loginRoutes = require('./routes/login')
 
 const port = process.env.PORT || 4000
 
-const corsOptions = {
-    origin: 'https://modernminesweeper.web.app/'
-}
-app.use(cors(corsOptions))
+app.use(cors())
+app.options('*', cors())
 app.use(express.json())
 app.use('/scores', scoresRoutes)
 app.use('/highscores', highScoresRoutes)
 app.use('/users', usersRoutes)
-app.use('/login', cors(), loginRoutes)
+app.use('/login', loginRoutes)
 
 app.listen(port)
